be9eee12d3315812858c04b7615c9cda4923b3df
Fer-gi <gise.fernandez87@gmail.com>
2023-06-27 20:49:39 +0200

    prueba de js

---
+++ main.js
@@ -0,0 +1,10 @@
+let temperature = 26;
+    if (temperature >=30){
+        console.log ("esto está insopotable")
+    }
+    else if ( temperature <=29 && temperature>=27){
+        console.log (" está aceptable")
+     }
+     else{
+        console.log ("temperatura ideal")
+     }
